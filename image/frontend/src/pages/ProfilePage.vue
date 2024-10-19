1ab2bfe9edc774e0dba312a3ddf3a3eedcf54266
ymjoz <rossiblack@duck.com>
2024-10-19 10:24:18 +0800

    chore: add Isreal Flag

--- image/frontend/src/pages/ProfilePage.vue
+++ image/frontend/src/pages/ProfilePage.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <h1>hello Porfile</h1>
+    <h2>
+      聯合利劍B
+    </h2>
+    <dir>
+      <img src="../assets/Israel.svg" alt="Israel Flag">
+    </dir>
   </div>
 </template>
 

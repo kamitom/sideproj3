b215a76f8bd12150465131d090bdb7b2b31f548f
ymjoz <rossiblack@duck.com>
2024-10-21 14:26:51 +0800

    refactor: 抽離PostList與PostItem

---
+++ image/frontend/src/components/PostItem.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="postItem">
+    <img src="" alt="" width="100%" height="100%" style="background: #eee" />
+    <div class="postInfo">
+      <div class="postMeta">
+        <TheAvatar />
+        <span>陳詩偉</span>
+        <span class="postPubDate">8.1小時之前發佈</span>
+        <PostActions />
+      </div>
+      <div class="postDesc">
+        <p>這是一棵樹。這是一棵樹。這是一棵樹。這是一棵樹。這是一棵樹。這是一棵樹。這是一棵樹。這是一棵樹。
+          #植物#樹#綠色#樹葉#特別#長的#標籤#省略#顯示..
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import TheAvatar from '../components/TheAvatar.vue'
+  import PostActions from '../components/PostActions.vue';
+
+</script>
+
+<style scoped>
+
+
+  .postActions {
+    grid-area: actions;
+    justify-self: end;
+  }
+
+  .postItem {
+    box-shadow: 0px 12px 24px rgba(0, 0, 0, 0.08);
+    border-radius: 8px;
+  }
+
+  .postInfo {
+    padding: 24px;
+  }
+
+  .postItem>img {
+    width: 100%;
+    height: 400px;
+    object-fit: cover;
+    background: #eee;
+    cursor: pointer;
+  }
+
+  .postMeta {
+    display: grid;
+    grid-template-areas:
+      "avatar name actions"
+      "pubDate pubDate actions";
+    grid-template-columns: 42px 1fr 3fr;
+    row-gap: 6px;
+  }
+
+  .postMeta .avatar {
+    grid-area: avatar;
+  }
+
+  .postMeta .postPubDate {
+    grid-area: pubDate;
+    color: #9f9f9f;
+    font-size: 14px;
+  }
+
+  .postActions {
+    grid-area: actions;
+    justify-self: end;
+  }
+
+  .postDesc {
+    margin-top: 28px;
+    white-space: pre-line;
+  }
+
+</style>
--- image/frontend/src/pages/HomePage.vue
+++ image/frontend/src/pages/HomePage.vue
@@ -1,96 +1,21 @@
 <template>
   <div>
-    <div class="postList">
-      <div class="postItem" v-for="n in 9" :key="n">
-        <img src="" alt="" width="100%" height="100%" style="background: #eee" />
-        <div class="postInfo">
-          <div class="postMeta">
-            <TheAvatar />
-            <span>陳詩偉</span>
-            <span class="postPubDate">9小時之前發佈</span>
-            <PostActions />
-          </div>
-          <div class="postDesc">
-            <p>這是一棵樹。這是一棵樹。這是一棵樹。這是一棵樹。這是一棵樹。這是一棵樹。這是一棵樹。這是一棵樹。
-              #植物#樹#綠色#樹葉#特別#長的#標籤#省略#顯示..
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <PostList>
+      <PostItem v-for="n in 5" :key="n" />
+    </PostList>
     <!-- <PostDetails /> -->
-    <PostUpload />
+    <!-- <PostUpload /> -->
     <!-- <HelloTeleport /> -->
   </div>
 </template>
 
 <script setup>
-  import TheIcon from '../components/TheIcon.vue'
-  import TheAvatar from '../components/TheAvatar.vue'
-  import PostDetails from '../components/PostDetails.vue';
-  import PostActions from '../components/PostActions.vue';
-  import PostUpload from '../components/PostUpload.vue';
+  // import PostUpload from '../components/PostUpload.vue';
+  import PostList from '../components/PostList.vue';
+  import PostItem from '../components/PostItem.vue';
   // import HelloTeleport from '../components/HelloTeleport.vue';
+  import PostUpload from '../components/PostUpload.vue';
 
 </script>
 
-<style scoped>
-
-
-  .postActions {
-    grid-area: actions;
-    justify-self: end;
-  }
-
-  .postItem {
-    box-shadow: 0px 12px 24px rgba(0, 0, 0, 0.08);
-    border-radius: 8px;
-  }
-
-  .postInfo {
-    padding: 24px;
-  }
-
-  .postItem>img {
-    width: 100%;
-    height: 400px;
-    object-fit: cover;
-    background: #eee;
-    cursor: pointer;
-  }
-
-  .postMeta {
-    display: grid;
-    grid-template-areas:
-      "avatar name actions"
-      "pubDate pubDate actions";
-    grid-template-columns: 42px 1fr 3fr;
-    row-gap: 6px;
-  }
-
-  .postMeta .avatar {
-    grid-area: avatar;
-  }
-
-  .postMeta .postPubDate {
-    grid-area: pubDate;
-    color: #9f9f9f;
-    font-size: 14px;
-  }
-
-  .postActions {
-    grid-area: actions;
-    justify-self: end;
-  }
-
-  .postDesc {
-    margin-top: 28px;
-    white-space: pre-line;
-  }
-
-  .postList {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    gap: 48px;
-  }
-</style>
+<style scoped></style>

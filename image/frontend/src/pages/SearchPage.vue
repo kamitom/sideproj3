b20dd9ea9e3239e3a0ca46b454cae9f6592b5b56
ymjoz <rossiblack@duck.com>
2024-10-21 15:08:55 +0800

    feat: 搜尋頁面update

--- image/frontend/src/pages/SearchPage.vue
+++ image/frontend/src/pages/SearchPage.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
-    <h1>hello search page</h1>
+    <h2 class="title">搜索結果: 植物</h2>
+    <PostList>
+      <PostItem v-for="n in 5" :key="n" />
+    </PostList>
   </div>
 </template>
 
 <script setup>
+  import PostList from '../components/PostList.vue';
+  import PostItem from '../components/PostItem.vue';
 
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+
+  .title {
+    font-weight: 600;
+    font-size: 24px;
+    margin-bottom: 44px;
+
+  }
+
+</style>
